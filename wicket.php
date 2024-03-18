895ca17d2878f556fd93feaaa1764e47752d25ba
Alex Masliychuk <alexmasliychuk@gmail.com>
2024-03-18 21:50:52 +0200

    - eliminate global variables function and constants and move them to the top - load text-domain file (even if it doesn't exist for now) - prevent the main plugin from being accessed directly - use plugin-exclusive "wicket-memberships" text domain - prevent "/includes" directory to be accessed directory

--- wicket.php
+++ wicket.php
@@ -4,11 +4,11 @@ namespace WicketMember;
 /**
  * Plugin Name: Wicket - Memberships
  * Plugin URI: http://wicket.io
- * Description: Wicket memberships addon to provide memberships functionality 
+ * Description: Wicket memberships addon to provide memberships functionality
  * Version: 0.0.1
  * Author: Wicket Inc.
  * Author URI: https://wicket.io/
- * Text Domain: wicket
+ * Text Domain: wicket-memberships
  * Domain Path: /languages
  * Requires at least: 6.0
  * Requires PHP: 8.2
@@ -16,8 +16,33 @@ namespace WicketMember;
  * @package Wicket
  */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
+}
+
+if ( ! defined( 'WICKET_MEMBER_URL' ) ) {
+	define( 'WICKET_MEMBER_URL', plugin_dir_url( __FILE__ ) );
+}
+
+if ( ! defined( 'WICKET_MEMBER_BASENAME' ) ) {
+	define( 'WICKET_MEMBER_BASENAME', plugin_basename( __FILE__ ) );
+}
+
+if ( ! defined( 'WICKET_MEMBER_PLUGIN_DIR' ) ) {
+	define( 'WICKET_MEMBER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'WICKET_MEMBER_PLUGIN_FILE' ) ) {
+	define( 'WICKET_MEMBER_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'WICKET_MEMBER_PLUGIN_SLUG' ) ) {
+	define( 'WICKET_MEMBER_PLUGIN_SLUG', 'wicket_member_wp' );
 }
 
 if ( ! class_exists( 'Wicket_Memberships' ) ) {
@@ -33,34 +58,22 @@ if ( ! class_exists( 'Wicket_Memberships' ) ) {
 	class Wicket_Memberships {
 
     public function __construct() {
-			$this->wicket_global_constants_vars();
 			include_once WICKET_MEMBER_PLUGIN_DIR . 'includes/wicket-member-controller.php';
 			include_once WICKET_MEMBER_PLUGIN_DIR . 'includes/wicket-member-register-controller.php';
 			include_once WICKET_MEMBER_PLUGIN_DIR . 'includes/wicket-member-wp-rest-controller.php';
-			register_activation_hook( __FILE__, array( $this, 'wicket_member_activate' ) );
-	
+			register_activation_hook( WICKET_MEMBER_PLUGIN_FILE, array( $this, 'plugin_activate' ) );
+			add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
     }
 
 		/**
 		 * Plugin activation config
 		 */
-		public function wicket_member_activate() {
+		public function plugin_activate() {
 			// Default settings for plugin.
 		}
 
-		/**
-		 * Define Global variables
-		 */
-		public function wicket_global_constants_vars() {
-			if ( ! defined( 'WICKET_MEMBER_URL' ) ) {
-				define( 'WICKET_MEMBER_URL', plugin_dir_url( __FILE__ ) );
-			}
-			if ( ! defined( 'WICKET_MEMBER_BASENAME' ) ) {
-				define( 'WICKET_MEMBER_BASENAME', plugin_basename( __FILE__ ) );
-			}
-			if ( ! defined( 'WICKET_MEMBER_PLUGIN_DIR' ) ) {
-				define( 'WICKET_MEMBER_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-			}
+		public function load_textdomain() {
+			load_plugin_textdomain( 'wicket-memberships', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 		}
 
 	} // end Class Wicket_Memberships.
