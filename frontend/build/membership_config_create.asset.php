24eea459fbbecf7914fbc34b825d084989db44b1
Alex Masliychuk <alexmasliychuk@gmail.com>
2024-04-07 18:19:02 +0300

    Backend validation implemented

--- frontend/build/membership_config_create.asset.php
+++ frontend/build/membership_config_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '98deae9ae8558e3aeded');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '1a27a1b1f39e23a105dd');
