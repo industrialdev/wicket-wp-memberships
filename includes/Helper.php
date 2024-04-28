299d5e518785c22213ad9f37ec136c6981994e2a
gpomer <25644123+gpomer@users.noreply.github.com>
2024-04-28 16:51:50 -0400

    update mdp membership dates
    apply rules  for various status transitions

--- includes/Helper.php
+++ includes/Helper.php
@@ -85,28 +85,7 @@ class Helper {
       ];
     }
   }
-  /*
-
-                'membership_parent_order_id' => $order_id,
-                'membership_subscription_id' => $subscription_id,
-                'membership_product_id' => $product_id,
-                'membership_tier_post_id' => $membership_tier->get_membership_tier_post_id(),
-                'membership_tier_name' => $membership_tier->tier_data['mdp_tier_name'],
-                'membership_tier_uuid' => $membership_tier->tier_data['mdp_tier_uuid'],
-                'membership_type' => $membership_tier->tier_data['type'],
-                'membership_starts_at' => $dates['start_date'],
-                'membership_ends_at' =>  $dates['end_date'],
-                'membership_expires_at' => !empty($dates['expires_at']) ? $dates['expires_at'] : $dates['end_date'],
-                'membership_early_renew_at' => !empty($dates['early_renew_at']) ? $dates['early_renew_at'] : $dates['end_date'],
-                'membership_period' => $period_data['period_type'],
-                'membership_interval' => $period_data['period_count'],
-                'membership_subscription_period' => get_post_meta( $subscription_id, '_billing_period')[0],
-                'membership_subscription_interval' => get_post_meta( $subscription_id, '_billing_interval')[0],
-                'membership_wp_user_id' => $user_object->ID,
-                'membership_wp_user_display_name' => $user_object->display_name,
-                'membership_wp_user_email' => $user_object->user_email
-  */
-
+  
   public static function get_membership_post_data_from_membership_json( $membership_json ) {
     $membership_post_data = array();
     $membership_array = json_decode( $membership_json, true);
@@ -136,5 +115,36 @@ class Helper {
       }
     );  
     return $membership_post_data;
+  }
+
+  public static function get_membership_json_from_membership_post_data( $membership_array ) {
+    $membership_json_data = array();
+    $mapping_keys = [
+        'member_type' => 'membership_type',
+        'start_date' => 'membership_starts_at',
+        'end_date' => 'membership_ends_at',
+        'expiry_date' => 'membership_expires_at',
+        'early_renew_date' => 'membership_early_renew_at',
+        'wicket_uuid' => 'membership_wicket_uuid',
+        'user_name' => 'membership_wp_user_display_name',
+        'user_email' => 'membership_wp_user_email',
+        'membership_order_id' => 'membership_parent_order_id',
+        'org_name' => 'organization_name',
+        'org_uuid' => 'organization_uuid',
+        'org_seats' => 'membership_seats',
+    ];
+    array_walk(
+      $membership_array,
+      function(&$val, $key) use (&$membership_json_data, $mapping_keys)
+      {
+        $new_key = $mapping_keys[$key];
+        if( empty($new_key) ) {
+          return;
+        }
+        $membership_json_data[$new_key] = $val;
+      }
+    );  
+    $membership_json = json_encode( $membership_json_data );
+    return $membership_json;
   }
 }
