4c28694adf2ec2c252e60704791067aa63e19cb9
gpomer <25644123+gpomer@users.noreply.github.com>
2024-05-13 08:39:44 -0400

    fix get membership entity with no WC records, return order and subscr status

--- includes/Admin_Controller.php
+++ includes/Admin_Controller.php
@@ -295,18 +295,19 @@ class Admin_Controller {
       $membership_item['order'] = [];
       $membership_item['subscription'] = [];
 
-      if( !empty( $membership_item['data'] )) {
-        @$order = wc_get_order( $membership_item['data']['membership_parent_order_id'] );
-        @$membership_item['order']['id'] = $membership_item['data']['membership_parent_order_id'];
-        @$membership_item['order']['link'] = admin_url( '/post.php?action=edit&post=' . $membership_item['data']['membership_parent_order_id'] );
-        @$membership_item['order']['total'] = $order->get_total();
-        @$membership_item['order']['date_created'] =  $order->get_date_created()->format('Y-m-d');
-        @$membership_item['order']['date_completed'] = $order->get_date_completed()->format('Y-m-d');
-
-        @$sub = wcs_get_subscription( $membership_item['data']['membership_subscription_id'] );
-        @$membership_item['subscription']['id'] = $membership_item['data']['membership_subscription_id'];
-        @$membership_item['subscription']['link'] = admin_url( '/post.php?action=edit&post=' . $membership_item['data']['membership_subscription_id'] );
-        @$membership_item['subscription']['next_payment_date'] = (new \DateTime( date("Y-m-d", $sub->get_time('next_payment')), wp_timezone() ))->format('Y-m-d');
+      if( !empty( $membership_item['data']['membership_parent_order_id'] ) && !empty( $membership_item['data']['membership_subscription_id'] )) {
+        $order = wc_get_order( $membership_item['data']['membership_parent_order_id'] );
+        $membership_item['order']['id'] = $membership_item['data']['membership_parent_order_id'];
+        $membership_item['order']['link'] = admin_url( '/post.php?action=edit&post=' . $membership_item['data']['membership_parent_order_id'] );
+        $membership_item['order']['total'] = $order->get_total();
+        $membership_item['order']['status'] = $order->get_status();
+        $membership_item['order']['date_created'] =  $order->get_date_created()->format('Y-m-d');
+        $membership_item['order']['date_completed'] = $order->get_date_completed()->format('Y-m-d');
+        $sub = wcs_get_subscription( $membership_item['data']['membership_subscription_id'] );
+        $membership_item['subscription']['id'] = $membership_item['data']['membership_subscription_id'];
+        $membership_item['subscription']['link'] = admin_url( '/post.php?action=edit&post=' . $membership_item['data']['membership_subscription_id'] );
+        $membership_item['subscription']['status'] = $sub->get_status();
+        $membership_item['subscription']['next_payment_date'] = (new \DateTime( date("Y-m-d", $sub->get_time('next_payment')), wp_timezone() ))->format('Y-m-d');
       } else {
         $membership_item['data'] = Helper::get_membership_json_from_membership_post_data( $meta, false );
         $membership_item['data']['status'] = 'active';
