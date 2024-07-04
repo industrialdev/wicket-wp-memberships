dca7310c7029a477a96ce858fd759b085ba6e677
gpomer <25644123+gpomer@users.noreply.github.com>
2024-07-04 14:46:19 -0400

    update php version in plugin file

--- wicket.php
+++ wicket.php
@@ -11,7 +11,7 @@ namespace Wicket_Memberships;
  * Text Domain: wicket-memberships
  * Domain Path: /languages
  * Requires at least: 6.0
- * Requires PHP: 8.2
+ * Requires PHP: 8.0
  *
  * @package Wicket
  */
