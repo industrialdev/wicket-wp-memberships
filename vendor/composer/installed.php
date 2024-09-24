c431ce696ed948ed227b6a038360865cc46ebbad
gpomer <25644123+gpomer@users.noreply.github.com>
2024-09-24 17:11:32 -0400

    include vendor folder for autoloading

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6a31181558adc5003b48beec1c973ec331ff60f2',
+    'name' => 'industrialdev/wicket-wp-memberships',
+  ),
+  'versions' => 
+  array (
+    'industrialdev/wicket-wp-memberships' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a31181558adc5003b48beec1c973ec331ff60f2',
+    ),
+  ),
+);
