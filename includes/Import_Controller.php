9d59f6e9f44c716b1a977f39d110830c8d28692d
gpomer <25644123+gpomer@users.noreply.github.com>
2024-10-22 13:53:05 -0400

    import create user if not exists

--- includes/Import_Controller.php
+++ includes/Import_Controller.php
@@ -33,6 +33,10 @@ class Import_Controller {
 
 
           $user = get_user_by('login', $record['Person_UUID']);
+          if(empty($user)) {
+            $user_id = wicket_create_wp_user_if_not_exist($record['Membership_Owner_UUID'], );
+            $user = get_user_by('id', $user_id);
+          }
           $membership_post_mapping['user_id'] = $user->ID;
           if( $user->display_name == $record['Person_UUID'] ) {
             $membership_post_mapping['membership_wp_user_display_name'] = $user->first_name . ' ' . $user->last_name;
@@ -85,6 +89,10 @@ class Import_Controller {
           }
 
           $user = get_user_by('login', $record['Membership_Owner_UUID']);
+          if(empty($user)) {
+            $user_id = wicket_create_wp_user_if_not_exist($record['Membership_Owner_UUID'], );
+            $user = get_user_by('id', $user_id);
+          }
           $membership_post_mapping['user_id'] = $user->ID;
           $membership_post_mapping['membership_wp_user_display_name'] = $user->display_name;
     	    $membership_post_mapping['membership_wp_user_email'] = $user->user_email;
