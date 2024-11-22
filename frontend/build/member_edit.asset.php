b1f6e7446891eade036155fe64d770d48021730e
Alex Masliychuk <alex@wicket.io>
2024-11-22 13:40:21 +0200

    initial

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'adaa24e0f322a1f48507');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '2ecc4b358da70df712fd');
