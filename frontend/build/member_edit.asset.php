9cadfc4e524360a0a4029c62942adff1d62c0300
Alex Masliychuk <alex@wicket.io>
2024-11-25 18:17:51 +0200

    Frontend logic implemented

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '2ecc4b358da70df712fd');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '9522e217d6e2b3df8ffe');
--- frontend/build/wicket_memberships_tier_cell_info.asset.php
+++ frontend/build/wicket_memberships_tier_cell_info.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'a65f5f872833e5ce3687');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'f5c1db4c4f60f91db761');
