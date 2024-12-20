2384710012b7fd138dab4975a204c9eeeb973035
gpomer <25644123+gpomer@users.noreply.github.com>
2024-12-20 13:00:21 -0500

    build scripts

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '1db64f16b611850bdbb1');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'ce37a4078a6a79d86306');
