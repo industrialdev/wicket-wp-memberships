3b371addcbe810010759329368a6485777fdc7cd
gpomer <25644123+gpomer@users.noreply.github.com>
2025-01-23 09:04:42 -0500

    run build scipts

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'e2ab08d47928d70cec9c');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '6a08c6084c3ba2a90d78');
--- frontend/build/membership_tier_create.asset.php
+++ frontend/build/membership_tier_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '1d3e33f33cd806453488');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'a71a99b9ef6340a39502');
