fe08680bd0b198e7eb24c91582ed72f5e52dfbc9
Alex Masliychuk <alex@wicket.io>
2025-02-07 14:13:34 +0200

    Add Membership Post ID to membership record view

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '92be828d741acb00273e');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '275de31aab2bb258dd27');
