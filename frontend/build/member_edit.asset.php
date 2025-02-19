fb3eb7ee1503ff426123cde2475618df746f84b1
gpomer <25644123+gpomer@users.noreply.github.com>
2025-02-19 14:42:46 -0500

    build scripts after merge

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '0a6a561e3cf8e342bda8');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'c58cfda3e9118962d581');
