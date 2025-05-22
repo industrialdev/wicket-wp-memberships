151d6dd54a428800d0f7ae1dfcca8687981b3640
Alex Masliychuk <alex@wicket.io>
2025-05-22 12:40:34 +0300

    "Create Renewal Order" improvements

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'd75f6ef42507802eb83b');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '5a1ce007e0505d4bc1ce');
