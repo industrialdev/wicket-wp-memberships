46225d4b05bb7555947c11da4f44325f2d8ed8bd
gpomer <25644123+gpomer@users.noreply.github.com>
2025-06-19 06:57:24 -0400

    build scripts

--- frontend/build/membership_config_create.asset.php
+++ frontend/build/membership_config_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '9a730af23e225460f4f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '3858c27aa6cdaaf3675b');
--- frontend/build/membership_tier_create.asset.php
+++ frontend/build/membership_tier_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '0f296078a1740ce4369b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '96751ae3f0f70aa0881b');
