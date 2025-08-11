acab8e5e6835ae24e285b03b0c47cbe2970585dd
Alex Masliychuk <alex@wicket.io>
2025-08-11 18:11:06 +0300

    Block "Update" button when the membership status is not selected

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '94c7e80e730a9041791c');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '22d9e73dd82fae050d2e');
