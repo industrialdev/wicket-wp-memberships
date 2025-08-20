fd2e22111d7a22240d15c35703c154c758043a18
gpomer <25644123+gpomer@users.noreply.github.com>
2025-08-20 17:51:17 -0400

    rebuild frontend scripts

--- frontend/build/tier_member_count.asset.php
+++ frontend/build/tier_member_count.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-url'), 'version' => '7cb36bed22f211f078bc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-url'), 'version' => 'f267dd36d9faf6f4c5de');
--- frontend/build/wicket_memberships_tier_cell_info.asset.php
+++ frontend/build/wicket_memberships_tier_cell_info.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'ca5e8a2e67bafb4d108d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'f5c1db4c4f60f91db761');
