42d931bb5443e7301b7f6cbef698055403d4b311
Alex Masliychuk <alex@wicket.io>
2026-01-21 14:14:24 +0200

    Remove 'Add Membership' Greyed Button from UI
    https://app.asana.com/1/1138832104141584/project/1212799428525736/task/1212827599628933

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'a89f6b3bf6185e64c3a3');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '70ae0aca48c6f2490f38');
