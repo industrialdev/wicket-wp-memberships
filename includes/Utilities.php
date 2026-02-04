b9e11c33cb771b895410f71937440c814eadd891
EstebanForge <1696145+EstebanForge@users.noreply.github.com>
2026-02-04 16:06:48 -0300

    retrieve the correct subs data
    https://app.asana.com/1/1138832104141584/task/1213062783023157?focus=true

--- includes/Utilities.php
+++ includes/Utilities.php
@@ -275,10 +275,24 @@ class Utilities {
   }
 
   function wicket_sub_org_select_metabox( $post_type, $post ) {
-    $sub = wcs_get_subscription($post->ID);
+    if ( $post_type !== 'shop_subscription' || ! is_object( $post ) || empty( $post->ID ) ) {
+      return;
+    }
+
+    if ( ! function_exists( 'wcs_get_subscription' ) ) {
+      return;
+    }
+
+    if ( function_exists( 'wcs_is_subscription' ) && ! wcs_is_subscription( $post->ID ) ) {
+      return;
+    }
+
+    $sub = wcs_get_subscription( $post->ID );
+
     if(empty($sub)) {
       return;
     }
+
     $options = get_option( 'wicket_membership_plugin_options' );
     $categories_selected = $options['wicket_show_mship_order_org_search']['categorychoice'];
     $subscription_products = $sub->get_items();
@@ -313,7 +327,32 @@ class Utilities {
 
 function wicket_sub_org_select_callback( $subscription ) {
       $org_uuid = '';
-      $sub = wcs_get_subscription($subscription);
+      if ( ! function_exists( 'wcs_get_subscription' ) ) {
+        return;
+      }
+
+      if ( $subscription instanceof WC_Subscription ) {
+        $sub = $subscription;
+      } elseif ( $subscription instanceof WP_Post ) {
+        if ( empty( $subscription->ID ) || $subscription->post_type !== 'shop_subscription' ) {
+          return;
+        }
+        if ( function_exists( 'wcs_is_subscription' ) && ! wcs_is_subscription( $subscription->ID ) ) {
+          return;
+        }
+        $sub = wcs_get_subscription( $subscription->ID );
+      } elseif ( is_numeric( $subscription ) ) {
+        $subscription_id = absint( $subscription );
+        if ( ! $subscription_id ) {
+          return;
+        }
+        if ( function_exists( 'wcs_is_subscription' ) && ! wcs_is_subscription( $subscription_id ) ) {
+          return;
+        }
+        $sub = wcs_get_subscription( $subscription_id );
+      } else {
+        return;
+      }
       if(empty($sub)) {
         return;
       }
@@ -681,7 +720,7 @@ function wicket_sub_org_select_callback( $subscription ) {
 
     <script>
       jQuery(document).ready(function($) {
-          
+
           // Update the Gravity Forms hidden field text field "Autorenew Selected". It must also have a custom CSS class set to "autorenew_selected"
           // This is used mainly for confirmation redirects
           function sync_autorenew_field() {
@@ -697,7 +736,7 @@ function wicket_sub_org_select_callback( $subscription ) {
 
           $('#wicket-wc-toggle').on('change', function() {
               var isChecked = $(this).is(':checked') ? 1 : 0;
-              
+
               sync_autorenew_field();
               console.log('wicket-wc-toggle changed');
 
