a4891e5dc9552e7f4f84026bf541d7e996d9659f
gary-wicket <gary@wicket.io>
2026-04-07 12:58:20 -0400

    front-end re-build after timestamp merge

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '90c961ecf2365446bfa3');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '7cacf10a19460b0422d4');
