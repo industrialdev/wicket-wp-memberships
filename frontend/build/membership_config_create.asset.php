d253588301c19dee17dee650eac17fc6eb19cc44
gary-wicket <gary@wicket.io>
2026-04-23 09:23:07 -0400

    update scripts, fix global log call

--- frontend/build/membership_config_create.asset.php
+++ frontend/build/membership_config_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '614acbcef27c3f8c0e28');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '72b293474fab11859468');
