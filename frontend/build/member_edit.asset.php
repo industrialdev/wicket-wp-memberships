76d7fa6d10670e363acb6b2e09e68c99d4c0ffb6
gary-wicket <gary@wicket.io>
2026-05-15 08:30:08 -0400

    rebuild scripts frontend

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '7cacf10a19460b0422d4');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '258d1657b9f32aad3f0d');
