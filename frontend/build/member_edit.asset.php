38e8b6ae2e8666323b0fea5bcfe13317164d6804
gary-wicket <gary@wicket.io>
2026-05-29 10:17:23 -0400

    build scripts

--- frontend/build/member_edit.asset.php
+++ frontend/build/member_edit.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '6f0aff8c16fe2a3ef313');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '150c5de591ddf2f124f7');
--- frontend/build/membership_config_create.asset.php
+++ frontend/build/membership_config_create.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => 'eaccbf5ed1546b0647be');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-i18n', 'wp-url'), 'version' => '8e778f69dea3cc8e00f8');
